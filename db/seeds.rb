68aaca6815f72926050891d3817b4f803c6a96c7
anthony vieira <thony16@gmail.com>
2023-11-27 18:44:50 -0300

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# puts "Cleaning up database..."
+# Groups.destroy_all
+# Activities.destroy_all
+# User.destroy_all
+
+# puts "Rodando........."
+# Activity.create!(title: "bar do Bolinha", category: "bar", address: "rua não sei")
+
+
+# user2 = User.create!(email: "master1@master1.com", password: 123123)
+# user2.groups.create!(name: "rolê no bar do bolinha", category: "Bar")
+# user2.groups.activities.create(title: "Bar do Bolinha", category: "bar", address: "rua não sei", image: "100")
