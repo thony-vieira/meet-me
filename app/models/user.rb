9fcdb7e1ae92e1de1d6afe19b9b4d6c20c7614bd
CaioBrasetti <caiobrasetti@hotmail.com>
2023-11-28 11:21:22 -0300

    Add seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_many :groups
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :groups, dependent: :destroy
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
 end
