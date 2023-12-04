222208bdc274b7d32bc5b4de6c396d515e2c944e
CaioBrasetti <caiobrasetti@hotmail.com>
2023-12-04 15:58:01 -0300

    Implemetando a rota dos usuarios ao point central

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -32,6 +32,9 @@ class GroupsController < ApplicationController
       # trocar a cor do meio da rota - Ler a documentação mapbox
     }
     @markers.push(mid_marker)
+    @directions = @users_in_group.reject { |user| user.latitude.blank? || user.longitude.blank? }.map do |user|
+      get_direction(user.longitude, user.latitude, lng_mid, lat_mid)
+    end
   end
 
   def new
@@ -64,6 +67,16 @@ class GroupsController < ApplicationController
   end
 
   private
+
+  def get_direction(start_lng, start_lat, end_lng, end_lat)
+    base_url = "https://api.mapbox.com/directions/v5/"
+    profile = "mapbox/driving/"
+    url = "#{base_url}#{profile}#{start_lng},#{start_lat};#{end_lng},#{end_lat}?overview=full&geometries=geojson&access_token=#{ENV['MAPBOX_API_KEY']}"
+    response = HTTP.get(url).body.to_s
+    data = JSON.parse(response)
+    coords = data["routes"][0]["geometry"]["coordinates"]
+    return coords
+  end
 
   def group_params
     params.require(:group).permit(:name, :category)
